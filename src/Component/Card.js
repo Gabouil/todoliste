1ddcae878f0216c0ffb770f3183e79193b9803c7
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-03-17 11:59:07 +0100

    20/20

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -79,14 +79,13 @@ export default function Card({title, desc, ID, status, items, setitems}) {
         <div className="d-flex flex-column align-items-center justify-content-between mb-4" key={ID}>
             <div className={`${status ? "btn-success" : "btn-warning"} btn w-100 mb-2 d-flex align-items-center justify-content-center position-relative`} htmlFor={ID}>
                 {handleChangeCard()}
-                <div className="w-25 position-absolute end-0">
-                    <button className="btn w-50" onClick={() => changeCard()}>
-                        <img className="w-100" src="https://upload.wikimedia.org/wikipedia/commons/5/57/Noun_project_-_crayon.svg"/>
-                    </button>
-                </div>
             </div>
-            <button className={`${status ? "btn-warning" : "btn-success"} btn ml-2 w-25 mb-1`} onClick={() => handleStatus()}>{status ? "À faire":"Fait"} </button>
-            <button className="btn btn-danger ml-2 w-25 mb-1" onClick={() => deleteTodo()}>Supprimer</button>
+            <div className="w-100 gap-2 d-flex justify-content-center">
+                <button className={`${status ? "btn-warning" : "btn-success"} btn ml-2 w-25 mb-1`} onClick={() => handleStatus()}>{status ? "À faire":"Fait"} </button>
+                <button className="btn btn-danger ml-2 w-25 mb-1" onClick={() => deleteTodo()}>Supprimer</button>
+                <button className={`${modifCard ? "btn-secondary" : "btn-primary"} btn ml-2 w-25 mb-1`} onClick={() => changeCard()}> {modifCard ? "Annuler   ":"Modifier"}</button>
+            </div>
+
         </div>
     )
 }
