98abddd9e7913d76c5035e105b0e47d8e152b5be
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-03-17 09:47:32 +0100

    input

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -25,13 +25,12 @@ export default function Card({key, title, desc, ID, status, items, setitems}) {
     
     return (
         <div className="d-flex flex-column align-items-center justify-content-between mb-4" key={ID}>
-            <input type="checkbox" className="btn-check" id={ID} autoComplete="off" />
-            <label className={`${status ? "btn-success" : "btn-warning"} btn w-100 mb-2`} htmlFor={ID}>
+            <div className={`${status ? "btn-success" : "btn-warning"} btn w-100 mb-2`} htmlFor={ID}>
                 <div className="d-flex flex-column align-items-center justify-content-between mb-3">
                     <h1 className="w-100 h-25">{title}</h1>
                     <p className="w-100 h-75">{desc}</p>
                 </div>
-            </label>
+            </div>
             <button className={`${status ? "btn-warning" : "btn-success"} btn ml-2 w-25 mb-1`} onClick={() => handleStatus()}>{status ? "À faire":"Fait"} </button>
             <button className="btn btn-danger ml-2 w-25 mb-1" onClick={() => deleteTodo()}>Supprimer</button>
         </div>
