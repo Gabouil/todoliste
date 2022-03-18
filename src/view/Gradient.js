c3f504b972ac7d3768c75adbc322a14885bacb18
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-03-18 13:21:22 +0100

    radial / linear

--- src/view/Gradient.js
+++ src/view/Gradient.js
@@ -7,6 +7,8 @@ import Colors from "../Component/Gradient/Colors"
 export default function Gradient() {
     const [colorsList, setcolorsList] = useState([{id:1,hexa:"#c173c1"},{id:2,hexa:"#f39d9e"},{id:3,hexa:"#317c04"}])
     const [deg, setdeg] = useState(300)
+    const [type, settype] = useState(true)
+
     // const [gradientBackground, setgradientBackground] = useState(colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa)
 
     // const plusBreackpoint = (e) => {
@@ -34,24 +36,45 @@ export default function Gradient() {
 
 
     return(
-        <div    className="w-100 d-flex align-items-center justify-content-center flex-column"      style={{height: "100vh",
-                                                                                                background: "linear-gradient(" + deg + "deg, " + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )", 
-                                                                                                background: "-moz-linear-gradient(" + deg + "deg, " + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )",
-                                                                                                background: "-webkit-linear-gradient(" + deg + "deg, " + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )"}}>
+        <div    className="w-100 d-flex align-items-center justify-content-center flex-column" style={{height: "100vh",
+        // c'est pas beau mais ça marche bien sorry pour les yeux :D
+                                                                                                        background: type ?
+                                                                                                        "linear-gradient(" + deg + "deg, " + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )" 
+                                                                                                        : 
+                                                                                                        "radial-gradient(" + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )", 
+
+                                                                                                        background: type ?
+                                                                                                        "-moz-linear-gradient(" + deg + "deg, " + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )" 
+                                                                                                        : 
+                                                                                                        "-moz-radial-gradient(" + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )",
+
+                                                                                                        background: type ?
+                                                                                                        "-webkit-linear-gradient(" + deg + "deg, " + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )" 
+                                                                                                        : 
+                                                                                                        "-webkit-radial-gradient(" + colorsList[0].hexa + " , " + colorsList[1].hexa + " , " + colorsList[2].hexa + " )"}}>
             <div className="w-100 d-flex align-items-center justify-content-center gap-3 mb-3" >
                 <Colors key={colorsList[0].id} colorsList={colorsList[0]} colorsListComplet={colorsList} setcolorsList={setcolorsList} id={colorsList[0].id}/>
                 <Colors key={colorsList[1].id} colorsList={colorsList[1]} colorsListComplet={colorsList} setcolorsList={setcolorsList} id={colorsList[1].id}/>
                 <Colors key={colorsList[2].id} colorsList={colorsList[2]} colorsListComplet={colorsList} setcolorsList={setcolorsList} id={colorsList[2].id}/>
             </div>
-            <div className="w-100 d-flex align-items-center justify-content-center gap-3 mb-3">
-                <Angle deg={deg} setdeg={setdeg}/>
-            </div>
+            {type ? 
+                <div className="w-100 d-flex align-items-center justify-content-center gap-3 mb-3">
+                    <Angle deg={deg} setdeg={setdeg}/>
+                </div>
+                :
+                <div className="w-100 d-flex align-items-center justify-content-center gap-3 mb-3">
+                    <p>il est beau ce radial :0</p>
+                </div>
+                }
+
             <div className="w-100 d-flex align-items-center justify-content-center gap-3 mb-3">
                 <button className="btn btn-primary"
                         onClick={randomColor}
                 >random</button>
+                <button className="btn btn-secondary"
+                        onClick={() => settype(type ? false : true)}
+                >{type ? "radial" : "linear"}</button>
             </div>
-
         </div>
     )
 }
