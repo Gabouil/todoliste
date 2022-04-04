e5a8a03ac640b150f2f84f042c463da86eaaa551
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-04-04 14:23:02 +0100

    ça marche pas mais fuck

--- src/view/Meteo.js
+++ src/view/Meteo.js
@@ -7,40 +7,51 @@ function Meteo() {
   const [temperature, setTemperature] = useState(0);
   const [cityName, setCityName] = useState("");
   const [link, setlink] = useState("");
+  const [erreur, seterreur] = useState("");
   
 
-  const fetchWeather = async () => {
-
+  const fetchWeather = async (e) => {
+    e.preventDefault()
     try {
-      setlink(
-        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b310544dca3e468d1a8900a5861d1e02`
-      );  
 
-      const response = await fetch(link)
-      const json = await response.json()
-      setTemperature(Math.floor(json.main.temp-273.15));
-      setWeather(json.weather[0].main);
-      setLongitude(json.coord.lon)
-      setLatitude(json.coord.lat)
-      // console.log(json);
+      setTimeout( async () => {
+        setlink(
+          `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b310544dca3e468d1a8900a5861d1e02`
+        );  
+        const response = await fetch(link)
+        const json = await response.json()
+        setTemperature(Math.floor(json.main.temp-273.15));
+        setWeather(json.weather[0].main);
+        setLongitude(json.coord.lon)
+        setLatitude(json.coord.lat)
+        // console.log(json);
+        seterreur('')
+      }, 1);
+ 
     } catch (err) {
       // console.error(err);
+      seterreur('Ville non trouvé')
+
     }
   };
 
   return (
     <>
+      <form>
         <input 
           value={cityName}
           type="test"
           placeholder="Titre"
-          onChange={e => (setCityName(e.target.value), fetchWeather())}
+          onChange={e => (setCityName(e.target.value))}
           className="form-control mb-2"
         />
+        <button id="addToTodo" className="btn btn-primary" onClick={e => (fetchWeather(e))}>Ajouter</button>
+      </form>
         {/* <RechercheVile cityName={cityName} setCityName={setCityName()} fetchWeather={fetchWeather} /> */}
         {/* je ne sais pas pourquoi mais il me sort une boucle infini qui se lance quand je ne met pas le input ici */}
       <div className="meteo">
         <div className="app-container d-flex align-items-center justify-content-around flex-column text-center">
+          <h1>{erreur}</h1>
           <h1>{cityName}</h1>
           <h2>{temperature}ºC</h2>
           <h2>{weather}</h2>
